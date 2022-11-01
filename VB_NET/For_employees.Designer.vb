7c453221b1ea491b89c3f5d0d6aad45a89c47a2e
anongnat <anongnat6952@gmail.com>
2022-11-01 13:45:05 +0700

    dd

--- VB_NET/For_employees.Designer.vb
+++ VB_NET/For_employees.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class For_employees
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
--- VB_NET/Patient_details.vb
+++ VB_NET/Patient_details.vb
@@ -1,3 +1,3 @@
-﻿Public Class Form1
+﻿Public Class Patient_details
 
 End Class
